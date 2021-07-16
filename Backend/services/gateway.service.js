b6c6d83c30edaa0109ae0b0ec655c2f18bc30cf0
umangraval <umangraval2000@gmail.com>
2021-07-17 00:41:25 +0530

    defined base for actions

--- Backend/services/gateway.service.js
+++ Backend/services/gateway.service.js
@@ -1,50 +1,96 @@
 "use strict";
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 module.exports = {
-    name: "gateway",
-    settings: {
-        port: process.env.PORT || 3000,
+  name: "gateway",
+  settings: {
+    port: process.env.PORT || 3000,
+  },
+  methods: {
+    initRoutes(app) {
+      app.get("/", this.ping);
+      app.post("/register", this.register);
+      app.get("/update", this.update);
+      app.get("/list", this.list);
+      app.get("/delete", this.delete);
     },
-    methods: {
-        initRoutes(app) {
-            app.get("/", this.ping);
-        },
-        ping(req, res) {
-            return Promise.resolve()
-                .then(() => {
-                    return this.broker.call("webhooks.testing").then(movies => {
-                        res.send(movies);
-                    });
-                })
-                .catch(this.handleErr(res));
-        },
-        handleErr(res) {
-            return err => {
-                res.status(err.code || 500).send(err.message);
-            };
-        }
+    ping(req, res) {
+      return Promise.resolve()
+        .then(() => {
+          return this.broker.call("webhooks.testing").then((data) => {
+            res.send(data);
+          });
+        })
+        .catch(this.handleErr(res));
     },
-    created() {
-        const app = express();
-        app.use(bodyParser());
-        this.initRoutes(app);
-        this.app = app;
+    register(req, res) {
+      const targetUrl = req.body.targetUrl;
+      return Promise.resolve()
+        .then(() => {
+          return this.broker
+            .call("webhooks.register", { url: targetUrl })
+            .then((data) => {
+              res.send(data);
+            });
+        })
+        .catch(this.handleErr(res));
     },
-    started() {
-		this.app.listen(Number(this.settings.port), err => {
-			if (err) return this.broker.fatal(err);
-			this.logger.info(`Server started on port ${this.settings.port}`);
-		});
-
-	},
-	stopped() {
-		if (this.app.listening) {
-			this.app.close(err => {
-				if (err) return this.logger.error("Server closed", err);
-				this.logger.info("Server stopped!");
-			});
-		}
-	}
+    update(req, res) {
+      const payload = req.body;
+      return Promise.resolve()
+        .then(() => {
+          return this.broker
+            .call("webhooks.update", { payload })
+            .then((data) => {
+              res.send(data);
+            });
+        })
+        .catch(this.handleErr(res));
+    },
+    list(req, res) {
+      return Promise.resolve()
+        .then(() => {
+          return this.broker.call("webhooks.list").then((data) => {
+            res.send(data);
+          });
+        })
+        .catch(this.handleErr(res));
+    },
+    delete(req, res) {
+      const { Id } = req.body;
+      return Promise.resolve()
+        .then(() => {
+          return this.broker.call("webhooks.delete", { Id }).then((data) => {
+            res.send(data);
+          });
+        })
+        .catch(this.handleErr(res));
+    },
+    handleErr(res) {
+      return (err) => {
+        res.status(err.code || 500).send(err.message);
+      };
+    },
+  },
+  created() {
+    const app = express();
+    app.use(bodyParser());
+    this.initRoutes(app);
+    this.app = app;
+  },
+  started() {
+    this.app.listen(Number(this.settings.port), (err) => {
+      if (err) return this.broker.fatal(err);
+      this.logger.info(`Server started on port ${this.settings.port}`);
+    });
+  },
+  stopped() {
+    if (this.app.listening) {
+      this.app.close((err) => {
+        if (err) return this.logger.error("Server closed", err);
+        this.logger.info("Server stopped!");
+      });
+    }
+  },
 };
--- Backend/services/webhooks.service.js
+++ Backend/services/webhooks.service.js
@@ -1,10 +1,37 @@
-"use strict"; 
+"use strict";
+const crypto = require("crypto");
 
 module.exports = {
-    name: "webhooks",
-    actions: {
-        testing(ctx) {
-           return Promise.resolve({ msg: "Pong" });
-        },
+  name: "webhooks",
+  actions: {
+    testing(ctx) {
+      return Promise.resolve({ msg: "Pong" });
     },
+    register(ctx) {
+      const Id = crypto
+        .randomBytes(20)
+        .toString("hex")
+        .substr(0, 7);
+      const data = {
+        Id: Id,
+        targetUrl: ctx.params.url
+      };
+      return Promise.resolve({ Id, msg: "Registered webhook" });
+    },
+    list(ctx) {
+      return Promise.resolve({ msg: "list webhooks" });
+    },
+    update(ctx) {
+      const { Id, newTargetUrl } = ctx.params;
+      const data = {
+        Id: Id,
+        targetUrl: newTargetUrl
+      };
+      return Promise.resolve({ msg: "Updated webhook" });
+    },
+    delete(ctx) {
+      const { Id } = ctx.params;
+      return Promise.resolve({ msg: "Deleted webhook" });
+    }
+  }
 };
